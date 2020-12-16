f65b24377ed55962879d58e8f7cc9f76cad0b381
Angela Ambroz <aambroz@optoro.com>
2020-12-16 09:57:26 -0500

    Blackening

--- jupyter/startup.py
+++ jupyter/startup.py
@@ -14,20 +14,23 @@ from sqlalchemy import create_engine
 from matplotlib import pyplot as plt
 from datetime import datetime, timedelta
 from IPython.display import Javascript, display, HTML
+
 # import tunbridge as tb
 
-plt.style.use('bmh')
+plt.style.use("bmh")
 
-def alert(text: str = 'All done!'):  
+
+def alert(text: str = "All done!"):
     js = f'alert("{text}");'
     display(Javascript(js))
 
+
 def flatten_jsonb(row):
-    for key, value in row['request'].items():
+    for key, value in row["request"].items():
         row[f"request_{key}"] = value
 
-    if 'result' in row['response'].keys():
-        for key,value in row['response']['result'][0].items():
+    if "result" in row["response"].keys():
+        for key, value in row["response"]["result"][0].items():
             row[f"response_{key}"] = value
 
     return row
@@ -35,65 +38,68 @@ def flatten_jsonb(row):
 
 def df_shape(dataframe: pd.DataFrame) -> str:
     """Print the df shape nicely"""
-    print(f'rows\t{dataframe.shape[0]:,.0f}')
-    print(f'cols\t{dataframe.shape[1]:,.0f}')
+    print(f"rows\t{dataframe.shape[0]:,.0f}")
+    print(f"cols\t{dataframe.shape[1]:,.0f}")
 
 
-def pull_data(query: str, db_cnx, 
-    save: bool = True, impatience: int = 120) -> pd.DataFrame:
-    DATA_DIR = 'data'
+def pull_data(
+    query: str, db_cnx, save: bool = True, impatience: int = 120
+) -> pd.DataFrame:
+    DATA_DIR = "data"
 
     stime = time()
     data = pd.read_sql(query, db_cnx)
     data.rename(columns=lambda x: str.lower(x), inplace=True)
     etime = time()
-    lapsed = etime-stime
+    lapsed = etime - stime
 
     # Only alert if it takes more than 2 mins to pull
     if lapsed > impatience:
-        alert(f'That took {lapsed:.2f} seconds.')
-    
+        alert(f"That took {lapsed:.2f} seconds.")
+
     print(f"That took {lapsed:.2f} seconds")
     print(data.shape)
 
     if save and lapsed > impatience:
-        filetime = datetime.fromtimestamp(etime).strftime('%Y-%m-%d_%H:%M:%S')
+        filetime = datetime.fromtimestamp(etime).strftime("%Y-%m-%d_%H:%M:%S")
 
         if DATA_DIR not in os.listdir():
             os.mkdir(DATA_DIR)
 
-        data.to_csv(f'{DATA_DIR}/{filetime}_data.csv')
+        data.to_csv(f"{DATA_DIR}/{filetime}_data.csv")
 
     return data
 
+
 def figsize(x: int, y: int):
-    mpl.rcParams['figure.figsize'] = (x, y)
+    mpl.rcParams["figure.figsize"] = (x, y)
+
 
 def cnx(db: str):
     """Convenience function to connect to various Optoro db's
 
     db: ['dw', 'pg1', 'pg2', 'pv']
     """
-    if db == 'dw':
+    if db == "dw":
         engine = f'mysql+pymysql://{os.environ.get("MYSQL_USERNAME")}:{os.environ.get("MYSQL_PASSWORD")}@{os.environ.get("MYSQL_DB_URL")}/'
 
-    if db == 'pg1':
+    if db == "pg1":
         engine = f'postgres://{os.environ.get("PSQL_USERNAME")}:{os.environ.get("PSQL_PASSWORD")}@{os.environ.get("PSQL1_DB_URL")}'
-    
-    if db == 'pg2':
-        engine = f'postgres://{os.environ.get("PSQL_USERNAME")}:{os.environ.get("PSQL_PASSWORD")}@{os.environ.get("PSQL2_DB_URL")}'
-    
-    if db == 'pv':
-        engine = f'postgres://{os.environ.get("PV_USERNAME")}:{os.environ.get("PV_PASSWORD")}@{os.environ.get("PV_DB_URL")}' 
 
-    if db == 'snow':
+    if db == "pg2":
+        engine = f'postgres://{os.environ.get("PSQL_USERNAME")}:{os.environ.get("PSQL_PASSWORD")}@{os.environ.get("PSQL2_DB_URL")}'
+
+    if db == "pv":
+        engine = f'postgres://{os.environ.get("PV_USERNAME")}:{os.environ.get("PV_PASSWORD")}@{os.environ.get("PV_DB_URL")}'
+
+    if db == "snow":
         engine = snow.connect(
-            user=os.environ['SNOWFLAKE_UN'],
-            password=os.environ['SNOWFLAKE_PW'],
-            account=os.environ['SNOWFLAKE_ACCOUNT'],
-            role='SYSADMIN',
-            warehouse='DS'
-            )
+            user=os.environ["SNOWFLAKE_UN"],
+            password=os.environ["SNOWFLAKE_PW"],
+            account=os.environ["SNOWFLAKE_ACCOUNT"],
+            role="SYSADMIN",
+            warehouse="DS",
+        )
 
         cs = engine.cursor()
         cs.execute("USE DW;")
@@ -108,4 +114,4 @@ def what_loaded():
 
 
 # TODO:
-# alert("Loaded cnx(), alert(), figsize(), df_shape()")    
+# alert("Loaded cnx(), alert(), figsize(), df_shape()")
--- system/museum.py
+++ system/museum.py
@@ -1,4 +1,3 @@
-
 import os
 import random
 import datetime
@@ -8,7 +7,13 @@ import praw
 import requests
 
 from Foundation import NSURL
-from AppKit import NSWorkspace, NSScreen, NSWorkspaceDesktopImageScalingKey, NSImageScaleProportionallyUpOrDown, NSWorkspaceDesktopImageAllowClippingKey
+from AppKit import (
+    NSWorkspace,
+    NSScreen,
+    NSWorkspaceDesktopImageScalingKey,
+    NSImageScaleProportionallyUpOrDown,
+    NSWorkspaceDesktopImageAllowClippingKey,
+)
 
 
 # helpful:
@@ -17,26 +22,58 @@ from AppKit import NSWorkspace, NSScreen, NSWorkspaceDesktopImageScalingKey, NSI
 
 # Some globals
 DIR = os.getcwd()
-SUBREDDITS = ["museum", "ColorizedHistory",
-				"MacroPorn", "FuturePorn", "spaceporn",
-				"historyporn", "ImaginaryLandscapes", "movieposterporn",
-				"RetroFuturism", "imaginaryhistory",
-				"ImaginaryTechnology", "auroraporn", "LandscapeAstro",
-				"StarshipPorn", "AlbumArtPorn", "ArtPorn", "OrganizationPorn",
-				"MapPorn", "FossilPorn", "desertporn", "seaporn", "waterporn",
-				"WeatherPorn", "ViewPorn", "ExposurePorn", "HellscapePorn",
-				"GeekPorn", "AdrenalinePorn", "ClimbingPorn", "BonsaiPorn",
-				"MushroomPorn", "geologyporn", "minimalist_art", "romanticism",
-                "ancient_art", "Medievalart", "painting", "MuseumPorn", "SculpturePorn",
-                "BattlePaintings", "ArtefactPorn"]
-REDDIT_PW = os.environ['REDDIT_PW']
-REDDIT_UN = os.environ['REDDIT_UN']
-REDDIT_CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
-REDDIT_CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
-NASA_APOD_KEY = os.environ['NASA_APOD']
+SUBREDDITS = [
+    "museum",
+    "ColorizedHistory",
+    "MacroPorn",
+    "FuturePorn",
+    "spaceporn",
+    "historyporn",
+    "ImaginaryLandscapes",
+    "movieposterporn",
+    "RetroFuturism",
+    "imaginaryhistory",
+    "ImaginaryTechnology",
+    "auroraporn",
+    "LandscapeAstro",
+    "StarshipPorn",
+    "AlbumArtPorn",
+    "ArtPorn",
+    "OrganizationPorn",
+    "MapPorn",
+    "FossilPorn",
+    "desertporn",
+    "seaporn",
+    "waterporn",
+    "WeatherPorn",
+    "ViewPorn",
+    "ExposurePorn",
+    "HellscapePorn",
+    "GeekPorn",
+    "AdrenalinePorn",
+    "ClimbingPorn",
+    "BonsaiPorn",
+    "MushroomPorn",
+    "geologyporn",
+    "minimalist_art",
+    "romanticism",
+    "ancient_art",
+    "Medievalart",
+    "painting",
+    "MuseumPorn",
+    "SculpturePorn",
+    "BattlePaintings",
+    "ArtefactPorn",
+]
+REDDIT_PW = os.environ["REDDIT_PW"]
+REDDIT_UN = os.environ["REDDIT_UN"]
+REDDIT_CLIENT_ID = os.environ["REDDIT_CLIENT_ID"]
+REDDIT_CLIENT_SECRET = os.environ["REDDIT_CLIENT_SECRET"]
+NASA_APOD_KEY = os.environ["NASA_APOD"]
 NASA_APOD_URL = "https://api.nasa.gov/planetary/apod?api_key={}".format(NASA_APOD_KEY)
 TODAY = datetime.date.today().strftime("%Y%B%d")
 YESTERDAY = (datetime.date.today() - datetime.timedelta(1)).strftime("%Y%B%d")
+
 
 def download_pic(URL):
     urllib.request.urlretrieve(URL, "{}/{}.jpg".format(DIR, TODAY))
@@ -45,38 +82,47 @@ def download_pic(URL):
 def get_reddit():
     sub = random.choice(SUBREDDITS)
     print("Today's chosen subreddit is /r/{}!".format(sub))
-    r = praw.Reddit(client_id=REDDIT_CLIENT_ID,
-                     client_secret=REDDIT_CLIENT_SECRET,
-                     user_agent="osx:museum-desktop.personal-app:v0.1 (by /u/{})".format(REDDIT_UN),
-                     username=REDDIT_UN,
-                     password=REDDIT_PW)
-    good_url = [x.url for x in r.subreddit(SUB).top('month', limit=3) if "jpg" or "png" in x][0]
+    r = praw.Reddit(
+        client_id=REDDIT_CLIENT_ID,
+        client_secret=REDDIT_CLIENT_SECRET,
+        user_agent="osx:museum-desktop.personal-app:v0.1 (by /u/{})".format(REDDIT_UN),
+        username=REDDIT_UN,
+        password=REDDIT_PW,
+    )
+    good_url = [
+        x.url for x in r.subreddit(SUB).top("month", limit=3) if "jpg" or "png" in x
+    ][0]
     download_pic(good_url)
 
 
 def get_apod():
     r = requests.get(NASA_APOD_URL)
-    download_pic(r.json()['url'])
+    download_pic(r.json()["url"])
 
 
 # Deleting the old stuff
 def delete_olds():
     if os.path.isfile("{}/{}.jpg".format(DIR, YESTERDAY)):
-    	print("Deleting desktop background from {}.".format(YESTERDAY))
-    	os.remove("{}/{}.jpg".format(DIR, YESTERDAY))
+        print("Deleting desktop background from {}.".format(YESTERDAY))
+        os.remove("{}/{}.jpg".format(DIR, YESTERDAY))
     else:
-    	print("There is no desktop image from {}.".format(YESTERDAY))
+        print("There is no desktop image from {}.".format(YESTERDAY))
 
 
 # Setting the desktop background
 def change_desktop_background(file):
     print("Now changing desktop background...")
     # make image options dictionary
-    options = { NSWorkspaceDesktopImageScalingKey: NSImageScaleProportionallyUpOrDown, NSWorkspaceDesktopImageAllowClippingKey: True }
+    options = {
+        NSWorkspaceDesktopImageScalingKey: NSImageScaleProportionallyUpOrDown,
+        NSWorkspaceDesktopImageAllowClippingKey: True,
+    }
     ws = NSWorkspace.sharedWorkspace()
     file_url = NSURL.fileURLWithPath_(file)
     for screen in NSScreen.screens():
-    	(result, error) = ws.setDesktopImageURL_forScreen_options_error_(file_url, screen, options, None)
+        (result, error) = ws.setDesktopImageURL_forScreen_options_error_(
+            file_url, screen, options, None
+        )
 
 
 # get_reddit()
