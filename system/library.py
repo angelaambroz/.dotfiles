328d78fcebe1740d36e959e33d5d7d77fb419c4b
Angela Ambroz <aambroz@optoro.com>
2022-04-27 13:44:17 -0400

    goodreads script

---
+++ system/library.py
@@ -0,0 +1,32 @@
+"""
+Goodreads API is dying, so I guess I'll have to scrape. :| 
+
+Finding the most popular books in my social circle.
+
+Maybe:
+    1. ETL data from Goodreads into a local db somewhere
+    2. then do analytics against it (most popular, most read/least liked, etc)
+"""
+
+import os
+import selenium
+
+
+def connect_to_goodreads():
+    return None
+
+
+def get_friends():
+    return None
+
+
+def get_user_books():
+    return None
+
+
+def store_data():
+    return None
+
+
+def most_popular():
+    return None
