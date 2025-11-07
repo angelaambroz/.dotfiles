70da4ab8c697e79f45e7851d4dfee2834e2bc434
Angela Ambroz <briz@discord_work>
2025-11-07 13:21:04 -0500

    adding claude comments to nvim init lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,135 +1,130 @@
- -- Fallback color scheme
- vim.cmd('colorscheme default')
+-- Fallback color scheme
+vim.cmd('colorscheme default')
 
- -- Bootstrap lazy.nvim package manager
- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
- if not (vim.uv or vim.loop).fs_stat(lazypath) then
-   vim.fn.system({
-     "git",
-     "clone",
-     "--filter=blob:none",
-     "https://github.com/folke/lazy.nvim.git",
-     "--branch=stable",
-     lazypath,
-   })
- end
- vim.opt.rtp:prepend(lazypath)
+-- Bootstrap lazy.nvim package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
- --- Plugin Specifications
- require("lazy").setup({
-   -- LSP and Debugging
-   "neovim/nvim-lspconfig",
-   "mfussenegger/nvim-dap",
-   "mfussenegger/nvim-dap-python",
-   { "rcarriga/nvim-dap-ui",
-     dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
-     lazy = false,
-   },
-   { "folke/neodev.nvim", opts = {} },
+--- Plugin Specifications
+require("lazy").setup({
+  -- LSP and Debugging
+  "neovim/nvim-lspconfig",              -- LSP configuration (autocomplete, go-to-def, etc.) - NOT ACTUALLY CONFIGURED YET
+  "mfussenegger/nvim-dap",              -- Debug Adapter Protocol client (debugger core)
+  "mfussenegger/nvim-dap-python",       -- Python debugger integration for DAP
+  { "rcarriga/nvim-dap-ui",             -- Visual debugger UI (breakpoints panel, variables, stack trace)
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    lazy = false,
+  },
+  { "folke/neodev.nvim", opts = {} },   -- Better LSP support for Neovim's Lua API (helps with nvim config dev)
 
-   -- File Navigation and Search
-   { "nvim-telescope/telescope.nvim",
-     version = "0.1.4",
-     dependencies = {"nvim-lua/plenary.nvim"}
-   },
-   "nvim-lua/plenary.nvim",
-   "preservim/nerdtree",
-   "Xuyuanp/nerdtree-git-plugin",
+  -- File Navigation and Search
+  { "nvim-telescope/telescope.nvim",    -- Fuzzy finder (Ctrl-P for files, Ctrl-T for grep)
+    version = "0.1.4",
+    dependencies = {"nvim-lua/plenary.nvim"}
+  },
+  "nvim-lua/plenary.nvim",              -- Lua utility library (dependency for many plugins)
+  "preservim/nerdtree",                 -- File tree sidebar (classic file browser)
+  "Xuyuanp/nerdtree-git-plugin",        -- Shows git status in NERDTree
 
-   -- Git Integration
-   "airblade/vim-gitgutter",
-   "tpope/vim-fugitive",
+  -- Git Integration
+  "airblade/vim-gitgutter",             -- Shows git diff in the gutter (added/removed/modified lines)
+  "tpope/vim-fugitive",                 -- Git commands inside vim (:Git blame, :Git diff, etc.)
 
-   -- Appearance and UI
-   "joshdick/onedark.vim",
-   "vim-airline/vim-airline",
-   "preservim/tagbar",
+  -- Appearance and UI
+  "joshdick/onedark.vim",               -- OneDark color scheme (Atom editor colors)
+  "vim-airline/vim-airline",            -- Fancy statusline at bottom
+  "preservim/tagbar",                   -- Sidebar showing code structure (functions, classes) - <leader>tt
 
-   -- Code Editing and Formatting
-   { 'stevearc/conform.nvim', opts = {} },
-   "mattn/emmet-vim",
-   "tpope/vim-commentary",
-   "Exafunction/codeium.vim",
-   "szw/vim-maximizer",
-   "dense-analysis/ale",
+  -- Code Editing and Formatting
+  { 'stevearc/conform.nvim', opts = {} }, -- Auto-formatter (runs black/isort on save)
+  "mattn/emmet-vim",                    -- HTML/CSS abbreviation expansion (type div.class then expand)
+  "tpope/vim-commentary",               -- Toggle comments with gc motion
+  "Exafunction/codeium.vim",            -- AI code completion (Copilot alternative)
+  "szw/vim-maximizer",                  -- Maximize/restore current split
+  "dense-analysis/ale",                 -- Async linting (shows errors as you type)
 
-   -- Language Support
-   "vim-python/python-syntax",
-   "raimon49/requirements.txt.vim",
-   "lepture/vim-jinja",
-   "shmup/vim-sql-syntax",
+  -- Language Support
+  "vim-python/python-syntax",           -- Better Python syntax highlighting
+  "raimon49/requirements.txt.vim",      -- Syntax for requirements.txt files
+  "lepture/vim-jinja",                  -- Jinja2 template syntax
+  "shmup/vim-sql-syntax",               -- SQL syntax highlighting
 
-   -- Misc Utilities
-   "tpope/vim-sensible",
-   "vim-test/vim-test",
-   "simnalamburt/vim-mundo",
-   "junegunn/vim-emoji",
- })
+  -- Misc Utilities
+  "tpope/vim-sensible",                 -- Sensible defaults for vim settings
+  "vim-test/vim-test",                  -- Run tests from vim (NOT CONFIGURED)
+  "simnalamburt/vim-mundo",             -- Visual undo tree (see undo history as tree)
+  "junegunn/vim-emoji",                 -- Emoji completion :emoji_name:
+})
 
- -- Core Settings
- vim.g.mapleader = ","
- vim.opt.nu = true
- vim.wo.foldmethod = 'indent'
- vim.cmd("set foldlevel=99")
- -- Map 0 to jump to first non-blank character
- vim.keymap.set({'n', 'v'}, '0', '^', { noremap = true })
+-- Core Settings
+vim.g.mapleader = ","                   -- Leader key is comma
+vim.opt.nu = true                       -- Show line numbers
+vim.wo.foldmethod = 'indent'            -- Fold based on indentation
+vim.cmd("set foldlevel=99")             -- Start with all folds open
 
- -- Python Settings
- vim.g.python3_host_prog = "/usr/bin/python3"
- vim.g.python_highlight_all = 1
+-- Map 0 to jump to first non-blank character (instead of start of line)
+vim.keymap.set({'n', 'v'}, '0', '^', { noremap = true })
 
- -- Plugin Configurations
- require('dap-python').setup('~/.pyenv/shims/python')
- require("conform").setup({
-   formatters_by_ft = {
-     lua = { "stylua" },
-     python = { "isort", "black" },
-     javascript = { { "prettierd", "prettier" } },
-   },
- })
- require("neodev").setup({
-   library = { plugins = { "nvim-dap-ui" }, types = true },
- })
- local dapui = require("dapui")
- dapui.setup()
+-- Python Settings
+vim.g.python3_host_prog = "/usr/bin/python3"  -- Python provider path
+vim.g.python_highlight_all = 1                -- Enable all Python highlighting
 
- -- Formatting
- require("conform").setup({
-   formatters_by_ft = {
-     lua = { "stylua" },
-     python = { "isort", "black" },
-     javascript = { { "prettierd", "prettier" } },
-   },
- })
+-- Plugin Configurations
+require('dap-python').setup('~/.pyenv/shims/python')  -- Python debugger uses pyenv python
 
- -- Keybindings
- -- General
- vim.keymap.set("i", "jk", "<esc>:w<cr>")
+require("conform").setup({              -- Auto-formatter config
+  formatters_by_ft = {
+    lua = { "stylua" },
+    python = { "isort", "black" },      -- isort for imports, black for code
+    javascript = { { "prettierd", "prettier" } },
+  },
+})
 
- -- Telescope
- local telescope = require('telescope.builtin')
- vim.keymap.set('n', '<c-p>', telescope.find_files, {})
- vim.keymap.set('n', '<c-t>', telescope.live_grep, {})
+require("neodev").setup({               -- Lua LSP setup for neovim development
+  library = { plugins = { "nvim-dap-ui" }, types = true },
+})
 
- -- Debugger
- vim.keymap.set('n', '<F2>', require('dapui').toggle, {})
- vim.keymap.set('n', '<F3>', require('dap').toggle_breakpoint, {})
- vim.keymap.set('n', '<F4>', require('dap').continue, {})
- vim.keymap.set('n', '<F5>', require('dap').step_over, {})
- vim.keymap.set('n', '<F6>', require('dap').step_into, {})
- vim.keymap.set('n', '<F7>', require('dap').step_out, {})
+local dapui = require("dapui")
+dapui.setup()                           -- Initialize debugger UI
 
- -- Misc
- vim.keymap.set('n', '<leader>tt', ':TagbarToggle<CR>', { silent = true })
- vim.keymap.set('n', '<leader>r', ':w<CR>:!uv run %<CR>', { desc = 'Run Python file with uv' })
+-- Keybindings
+-- General
+vim.keymap.set("i", "jk", "<esc>:w<cr>")  -- jk in insert mode = escape and save
 
- -- Autocommands
- vim.api.nvim_create_autocmd("BufWritePre", {
-   pattern = "*",
-   callback = function(args)
-     require("conform").format({ bufnr = args.buf })
-   end,
- })
+-- Telescope
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<c-p>', telescope.find_files, {})  -- Ctrl-P: fuzzy find files
+vim.keymap.set('n', '<c-t>', telescope.live_grep, {})   -- Ctrl-T: grep search
 
- -- Theme
- vim.cmd("colorscheme onedark")
+-- Debugger (F-keys)
+vim.keymap.set('n', '<F2>', require('dapui').toggle, {})         -- F2: toggle debug UI
+vim.keymap.set('n', '<F3>', require('dap').toggle_breakpoint, {}) -- F3: set breakpoint
+vim.keymap.set('n', '<F4>', require('dap').continue, {})          -- F4: start/continue
+vim.keymap.set('n', '<F5>', require('dap').step_over, {})         -- F5: step over
+vim.keymap.set('n', '<F6>', require('dap').step_into, {})         -- F6: step into
+vim.keymap.set('n', '<F7>', require('dap').step_out, {})          -- F7: step out
+
+-- Misc
+vim.keymap.set('n', '<leader>tt', ':TagbarToggle<CR>', { silent = true })  -- ,tt: toggle tagbar
+vim.keymap.set('n', '<leader>r', ':w<CR>:!uv run %<CR>', { desc = 'Run Python file with uv' })  -- ,r: run with uv
+
+-- Autocommands
+vim.api.nvim_create_autocmd("BufWritePre", {  -- Format on save
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
+
+-- Theme
+vim.cmd("colorscheme onedark")
